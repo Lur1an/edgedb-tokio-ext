94f94693cd17ec3b5811b6b7c4bf99417c060c5b
lur1an <lurian-code@protonmail.com>
2024-04-29 16:59:21 +0200

    parsing now extracts projection and aliases

--- ext-derive/src/lib.rs
+++ ext-derive/src/lib.rs
@@ -2,22 +2,54 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, LitStr};
+use syn::{parse_macro_input, Data, DeriveInput, LitStr, Meta};
 
-fn derive_projection(input: DeriveInput) -> TokenStream {
+struct Projection<'a> {
+    field_name: &'a syn::Ident,
+}
+
+enum ProjectionType {
+    Alias(String),
+    Expression(String),
+    Nested(String),
+}
+
+fn derive_projection(input: DeriveInput) -> proc_macro2::TokenStream {
     let Data::Struct(data_struct) = input.data else {
         panic!("Project macro only applicable to structs");
     };
     let name = &input.ident;
-    // let mut projections = Vec::with_capacity(data_struct.fields.len());
-    data_struct.fields.iter().for_each(|field| {
+    let mut projections = Vec::with_capacity(data_struct.fields.len());
+    for field in &data_struct.fields {
         for attr in &field.attrs {
-            if !matches!(attr.style, syn::AttrStyle::Outer) {
+            let field_name = field.ident.as_ref().unwrap();
+            if !attr.path().is_ident("project") {
                 continue;
             }
-            if attr.path().is_ident("alias") {}
+            attr.parse_nested_meta(|meta| {
+                if meta.path.is_ident("alias") {
+                    let value = meta.value()?;
+                    let alias = value.parse::<LitStr>()?.value();
+                    if alias.is_empty() {
+                        return Err(meta.error("Expected non-empty alias"));
+                    }
+                    projections.push(format!("{} := .{}", field_name, alias));
+                } else if meta.path.is_ident("exp") {
+                    let value = meta.value()?;
+                    let edgedb_expression = value.parse::<LitStr>()?.value();
+                    if edgedb_expression.is_empty() {
+                        return Err(meta.error("Expected non-empty expression"));
+                    }
+                    projections.push(format!("{} := {}", field_name, edgedb_expression));
+                } else {
+                    return Err(meta.error("Expected alias or exp identifier"));
+                }
+                Ok(())
+            })
+            .unwrap();
         }
-    });
+    }
+    println!("{:?}", projections);
     let code = quote! {
         impl edgedb_tokio_ext::QueryProjection for #name {
             fn project() -> &'static str {
@@ -25,13 +57,13 @@ fn derive_projection(input: DeriveInput) -> TokenStream {
             }
         }
     };
-    code.into()
+    code
 }
 
-#[proc_macro_derive(Project, attributes(alias, exp))]
+#[proc_macro_derive(Project, attributes(project))]
 pub fn derive_projection_macro(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    derive_projection(input)
+    derive_projection(input).into()
 }
 
 #[proc_macro]
@@ -45,22 +77,23 @@ pub fn query_project(input: TokenStream) -> TokenStream {
 
 #[cfg(test)]
 mod test {
-    use syn::parse::Parse;
+    use syn::DeriveInput;
 
-    use super::*;
+    use super::derive_projection;
 
     #[test]
     fn test_macro_output() {
         let input = r#"
             struct User {
-                id: i32,
-                name: String,
+                #[project(alias = "id")]
+                user_id_value_what_am_i_doing: Uuid,
+                #[project(exp = ".org.name")]
+                org_name: String,
             }
         "#;
 
         let input: proc_macro2::TokenStream = syn::parse_str(input).unwrap();
         let derive_input: DeriveInput = syn::parse2(input).unwrap();
-        let output = derive_projection(derive_input).to_string();
-        println!("{}", output);
+        let output = derive_projection(derive_input);
     }
 }
